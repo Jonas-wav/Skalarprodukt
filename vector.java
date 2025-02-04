6aec0dea1e5a16d97e24eda3bf84d387c1f24737
Jonas-wav <jonassigle@gmx.de>
2025-02-04 16:47:37 +0100

    optimized with while(swap)

--- vector.java
+++ vector.java
@@ -13,14 +13,14 @@ public class vector {
         return scalar;
     }
 
-    public static int[] sort(int[] v1) {
+    public static void sort(int[] v1) {
 
-        int a = 0;
-        boolean swap = false;
+        int a;
+        boolean swap = true;
         int counter = 0;
-        sort:
-        for (int j = 0; j < v1.length; ++j) {
-            for (int i = 0; i < v1.length - 1; ++i) {
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < v1.length - 1 - counter; ++i) {
 
                 if (v1[i] > v1[i + 1]) {
                     a = v1[i];
@@ -30,15 +30,18 @@ public class vector {
                 }
                 
             }
+            counter++;
         }
-        return v1;
+    
     }
+
 
     public static void main(String[] args) {
         int[] v1 = {1, 2, 3, 34, 56, 54, 5, 0};
-        System.out.println(Arrays.toString(v1));
         int[] v2 = {4, 5, 6, 7};
-        System.out.println(Arrays.toString(sort(v1)));
+        System.out.println(Arrays.toString(v1));
+        sort(v1);
+        System.out.println(Arrays.toString(v1));
 
         // System.out.println("Skalarprodukt="+scalarProduct(v1, v2));
     }
